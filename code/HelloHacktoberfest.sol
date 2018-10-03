ab5f357ff7cacc9c1747f1af322275f822c2fd78
Gleb Skibitsky <gleb@dragonglass.com>
2018-10-03 22:36:22 +0300

    Create HelloHacktoberfest.sol

---
+++ code/HelloHacktoberfest.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.20;
+
+// Hacktober greetings smart contract
+contract HelloHacktoberfest {
+  
+  function Hello() external view returns (string) {
+    return "Hello Hacktoberfest 2018!";
+  }
+  
+}
