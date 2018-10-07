f795b1a2dbc5b47673592109404dc3d0c4f1fefa
Kanishk gupta <kanishkgupta1234@gmail.com>
2018-10-07 11:08:12 +0530

    Create HelloWOrld.cs

---
+++ code/HelloWOrld.cs
@@ -0,0 +1,10 @@
+// Hello World! program C#
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello HacktoberFest!");
+        }
+    }
+}
