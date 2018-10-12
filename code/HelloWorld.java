50988d7edc397acc4048270ec34148386fd144ca
Paras Aggarwal <paras.aggarwal@outlook.com>
2018-10-12 21:03:34 +0530

    Update HelloWorld.java

--- code/HelloWorld.java
+++ code/HelloWorld.java
@@ -1,6 +1,7 @@
-public class HelloWorld{
-
-  public static void main(String[] args){
-    System.out.println("Hello World from India!");
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Hello World from India!");
   }
 }
