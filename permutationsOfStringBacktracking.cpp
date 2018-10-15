3f0d2bbf9b90140e5b14a0ee5d8345fcf50734a2
abhisekch <ac1959@cse.jgec.ac.in>
2018-10-16 01:33:53 +0530

    Generating permutations of a string using backtracking

---
+++ permutationsOfStringBacktracking.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <string.h>
+
+void swap(char *x, char *y) 
+{ 
+    char temp; 
+    temp = *x; 
+    *x = *y; 
+    *y = temp; 
+} 
+
+void permute(char *a, int l, int r) 
+{ 
+   int i; 
+   if (l == r) 
+     printf("%s\n", a); 
+   else
+   { 
+       for (i = l; i <= r; i++) 
+       { 
+          swap((a+l), (a+i)); 
+          permute(a, l+1, r); 
+          swap((a+l), (a+i)); //backtrack 
+       } 
+   } 
+} 
+
+int main() 
+{ 
+    char str[] = "ABC"; 
+    int n = strlen(str); 
+    permute(str, 0, n-1); 
+    return 0; 
+}
