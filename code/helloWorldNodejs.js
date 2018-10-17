e98f97eaa4ab3ef8556f486c9ca62a0a4ba63970
Chiro Jacinto <jacinto@jacintoramirez.com>
2018-10-17 09:16:01 -0700

    Hacktoberfest Chiro

--- code/helloWorldNodejs.js
+++ code/helloWorldNodejs.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  res.send('Hola Mundo!');
+
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3030, function () {
+	
+  console.log('Ejemplo APP, escuchando por puerto 3030!');
+  
 });
