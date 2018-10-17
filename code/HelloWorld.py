8a07704df019547feebfda2871c5ff183d84dbb5
hfsf <hanniel88@gmail.com>
2018-10-17 15:49:51 -0300

    Added support for Python HelloWorld for Python version detection

--- code/HelloWorld.py
+++ code/HelloWorld.py
@@ -1,2 +1,8 @@
-#code for Hello World works only in Python 3 and above
-print ("Hello World")
+#code for Hello World detects python version and use the correct command accordingly
+
+import sys
+
+if float(sys.version) >= 3.:
+    exec('print ("Hello World")')
+else:
+    exec('print "Hello World"')
