1fb63f29fdf589b2bfb5585c12f08441c347eae4
VIDHI GUPTA <42618900+vidhiguptaa@users.noreply.github.com>
2018-10-19 18:21:58 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,6 @@
+#my first contributions towards hacktoberfest2018
+def display():
+  print("my name is vidhi\n")
+  print("this is my first contribution towards hacktoberfest\n")
+  print("learning python currently and further wants to explore machine learning \n")
+display()
