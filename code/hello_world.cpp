06b2102fbbcd68e88f8b691d2928d1ca69f311e8
Marco <42967229+Krammm@users.noreply.github.com>
2018-10-21 20:51:30 +0200

    Create hello_world.cpp

---
+++ code/hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Include the Input Output library
+
+using namespace std; // Include the Standard Namespace
+
+int main() { // Declare the main function
+
+  cout << "Hello, World."; // Print to the screen "Hello, World."
+
+  return 0; // Return 0
+
+} // End of main function
