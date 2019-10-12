1707cec902ecfeb14e247e5099e3b4497421521e
jkunal14 <43882559+jkunal14@users.noreply.github.com>
2019-10-13 00:17:16 +0530

    Update OddEven.c

--- code/OddEven.c
+++ code/OddEven.c
@@ -1,19 +1,13 @@
 #include<stdio.h>
 void main()
 {
-	
 	int a;
-	
 	printf("Enter a Number:");
 	scanf("%d",&a);
-	
+
 	if(a%2==0)
-	printf("The Entered Number is Even");
-	
+		printf("The Entered Number is Even");
 	else
-	printf("The Entered Number is Odd");
-	
-		
+		printf("The Entered Number is Odd");	
 	getch();
-	
 }
