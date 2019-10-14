ceaa05dad399ebb0304afb1e56a95031268a1850
Bhupen Pal <bhupen16pal@gmail.com>
2019-10-14 19:18:21 +0530

    Added Bhupen.cpp

---
+++ code/bhupen-hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //This is just a simple program to print hello world.
+    cout<<"Hello World";
+    //This is just a simple line to add a new line.
+    cout<<endl;
+    return 0;
+}
